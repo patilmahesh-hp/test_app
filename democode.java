2ab38e6f2bb5366c2249b35ae862a44445f8d38f
Mahesh Patil <maheshp@heaptrace.net>
2024-05-13 12:52:58 +0530

    add new file

---
+++ democode.java
@@ -0,0 +1,11 @@
+/**
+ * democode
+ */
+public class democode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is demo");
+        System.out.println("For git Setup");
+    }
+}
